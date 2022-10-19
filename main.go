c2000af326374a8d5108f39ef9ff181400464564
Olajumoke Tobi <Olajumoketobi01@gmail.com>
2022-10-20 00:15:28 +0100

    My Go Journey

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//String
+	var nameOne string = "Bankai"
+	var nameTwo = "katoom"
+	var nameThree string
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	var nameOnes = "Bank"
+	var nameThrees = "boy"
+	fmt.Println(nameOnes, nameTwo, nameThrees)
+
+	nameFour := 30
+	fmt.Println(nameFour)
+}
