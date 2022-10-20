9db6558d02fcf5e7adb59ff2657bbab2d2b2a4a2
Olajumoke Tobi <Olajumoketobi01@gmail.com>
2022-10-20 01:08:18 +0100

    learning integers

--- main.go
+++ main.go
@@ -3,18 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	//String
-	var nameOne string = "Bankai"
-	var nameTwo = "katoom"
-	var nameThree string
+	// //String
+	// var nameOne string = "Bankai"
+	// var nameTwo = "katoom"
+	// var nameThree string
 
-	fmt.Println(nameOne, nameTwo, nameThree)
+	// fmt.Println(nameOne, nameTwo, nameThree)
 
-	var nameOnes = "Bank"
-	var nameThrees = "boy"
-	fmt.Println(nameOnes, nameTwo, nameThrees)
+	// var nameOnes = "Bank"
+	// var nameThrees = "boy"
+	// fmt.Println(nameOnes, nameTwo, nameThrees)
 
-	nameFour := 30
+	// nameFour := 30
 
-	fmt.Println(nameFour)
+	// fmt.Println(nameFour)
+
+	var ageOne int = 10
+	var ageTwo = 30
+	agethree := 15
+
+	fmt.Println(ageOne, ageTwo, agethree)
 }
