3ead2de8f5509a4ada813392531df404426c4443
TobiOlajumoke <olajumoketobi01@gmail.com>
2022-10-21 23:38:40 +0100

    import strings uses

--- main.go
+++ main.go
@@ -55,33 +55,45 @@
 // 	fmt.Println("the saved string is :", str)
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	//  var ages [3]int = [3]int{20, 25, 30}
+// 	var ages = [3]int{20, 25, 30}
+// 	names := [4]string{"tobi", "ayo", "segun", "victor"}
+// 	names[1] = "bito"
+// 	fmt.Println(ages, len(ages))
+// 	fmt.Println(names, len(names))
+
+// 	// slices (use arrays under the hood)
+// 	var scores = []int{100, 50, 60}
+// 	scores[2] = 25
+// 	scores = append(scores, 85)
+
+// 	fmt.Println(scores, len(scores))
+
+// 	//slice ranges
+// 	rangeOne := names[1:3]
+// 	rangeTwo := names[2:]
+// 	rangeThree := names[:3]
+
+// 	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+// 	rangeOne = append(rangeOne, "koopa")
+// 	fmt.Println(rangeOne)
+
+// }
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	//  var ages [3]int = [3]int{20, 25, 30}
-	var ages = [3]int{20, 25, 30}
-	names := [4]string{"tobi", "ayo", "segun", "victor"}
-	names[1] = "bito"
-	fmt.Println(ages, len(ages))
-	fmt.Println(names, len(names))
-
-	// slices (use arrays under the hood)
-	var scores = []int{100, 50, 60}
-	scores[2] = 25
-	scores = append(scores, 85)
-
-	fmt.Println(scores, len(scores))
-
-	//slice ranges
-	rangeOne := names[1:3]
-	rangeTwo := names[2:]
-	rangeThree := names[:3]
-
-	fmt.Println(rangeOne, rangeTwo, rangeThree)
-
-	rangeOne = append(rangeOne, "koopa")
-	fmt.Println(rangeOne)
+	greeting := "hello there friends!"
+	fmt.Println(strings.Contains(greeting, "hello!"))
 
 }
