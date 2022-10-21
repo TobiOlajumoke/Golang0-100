8e063d639769e0e8ce9213c8472e96376069a788
TobiOlajumoke <olajumoketobi01@gmail.com>
2022-10-21 11:09:08 +0100

    Print and Sprint formats

--- main.go
+++ main.go
@@ -3,29 +3,59 @@ package main
 import "fmt"
 
 func main() {
-	// //String
-	// var nameOne string = "Bankai"
-	// var nameTwo = "katoom"
-	// var nameThree string
+	age := 22
+	name := "Tobi"
+	// // //String
+	// // var nameOne string = "Bankai"
+	// // var nameTwo = "katoom"
+	// // var nameThree string
 
-	// fmt.Println(nameOne, nameTwo, nameThree)
+	// // fmt.Println(nameOne, nameTwo, nameThree)
 
-	// var nameOnes = "Bank"
-	// var nameThrees = "boy"
-	// fmt.Println(nameOnes, nameTwo, nameThrees)
+	// // var nameOnes = "Bank"
+	// // var nameThrees = "boy"
+	// // fmt.Println(nameOnes, nameTwo, nameThrees)
 
-	// nameFour := 30
+	// // nameFour := 30
 
-	// fmt.Println(nameFour)
+	// // fmt.Println(nameFour)
 
-	// int
-	var ageOne int = 10
-	var ageTwo = 30
-	agethree := 15
+	// // int
+	// var ageOne int = 10
+	// var ageTwo = 30
+	// agethree := 15
 
-	fmt.Println(ageOne, ageTwo, agethree)
-	// // bits & memory
-	// var numOne int8 = 25
-	// var numTwo int8 = -128
-	// var numThree uint16 = 256
+	// fmt.Println(ageOne, ageTwo, agethree)
+
+	// // // bits & memory
+
+	// // var numOne int8 = 25
+	// // var numTwo int8 = -128
+	// // var numThree uint16 = 256
+
+	// var scoreOne float32 = 25.98
+	// var scoreTwo float64 = 122345667
+	// scoreThree := 3.4444
+	// fmt.Println(scoreOne, scoreTwo, scoreThree)
+
+	//Print
+	fmt.Print("hello, ")
+	fmt.Print("World! \n")
+	fmt.Print("new line \n")
+	//Println
+	fmt.Println("hello ninjas")
+	fmt.Println("goodbye ninjas")
+	fmt.Println("my age is", age, "and my name is", name)
+
+	// Printf (formatted strings) %_ = format specifier
+
+	fmt.Printf("my age is %v and my name is %v\n", age, name)
+	fmt.Printf("my age is %q and my name is %q\n", age, name)
+	fmt.Printf("age is of type %T\n", age)
+	fmt.Printf("you scored %f points! \n", 225.55)
+	fmt.Printf("you scored %0.1f points! \n", 225.55)
+
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v\n", age, name)
+	fmt.Println("the saved string is :", str)
 }
