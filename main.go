d1c0b7f64cf1499b31d43945906a5ab926a40514
TobiOlajumoke <olajumoketobi01@gmail.com>
2022-10-22 00:16:13 +0100

    The sort allows for easy arrangements of int 'ages' here

--- main.go
+++ main.go
@@ -89,19 +89,23 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sort"
 )
 
 func main() {
-	greeting := "hello there friends!"
-	// fmt.Println(strings.Contains(greeting, "hello!"))
-	// fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
-	// fmt.Println(strings.ToUpper(greeting))
-	// fmt.Println(strings.Index(greeting, "ll"))
-	// fmt.Println(strings.Index(greeting, "th"))
-	fmt.Println(strings.Split(greeting, " "))
+	// greeting := "hello there friends!"
+	// // fmt.Println(strings.Contains(greeting, "hello!"))
+	// // fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	// // fmt.Println(strings.ToUpper(greeting))
+	// // fmt.Println(strings.Index(greeting, "ll"))
+	// // fmt.Println(strings.Index(greeting, "th"))
+	// fmt.Println(strings.Split(greeting, " "))
 
-	// the original value is unchanged
-	fmt.Println("orignal string value =", greeting)
+	// // the original value is unchanged
+	// fmt.Println("orignal string value =", greeting)
+
+	ages := []int{45, 20, 35, 75, 60, 50, 25}
+	sort.Ints(ages)
+	fmt.Println(ages)
 
 }
