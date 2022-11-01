52af5b9ec1d2a8163cc61ececc790aa0fa0d3c82
TobiOlajumoke <olajumoketobi01@gmail.com>
2022-11-01 01:56:39 +0100

    Floats

---
+++ data-types-and-variables/04_floats/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 float32 = 3.1414141441414141414144141441
+	fmt.Println(num1)
+
+	var num2 float64 = 3.1411414141414441411414141414
+	fmt.Println(num2)
+}
