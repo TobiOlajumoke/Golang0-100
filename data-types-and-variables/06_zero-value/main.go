6a4d567c865c0d68f7dcf8737c7a1399708018c0
TobiOlajumoke <olajumoketobi01@gmail.com>
2022-11-01 23:22:26 +0100

    zero-value

---
+++ data-types-and-variables/06_zero-value/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var Iscool bool    // false
+	var num int        // 0
+	var message string // ""
+	fmt.Println(Iscool)
+	fmt.Println(num)
+	fmt.Println(message)
+}
